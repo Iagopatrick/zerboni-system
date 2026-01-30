97f7a37e3804ee70768413b2500f6b0504e67ef9
Iagopatrick <iagogripp@hotmail.com>
2026-01-29 23:28:24 -0300

    feat: add navBar and home template

---
+++ src/constans/colors.ts
@@ -0,0 +1,5 @@
+export const colors= {
+    primary: "#0277A0",
+    secondary: "#E17800",
+    selected: "#F9F9F9",
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,16 @@
+// tailwind.config.js
+export default {
+  theme: {
+    extend: {
+      colors: {
+        primary: 'var(--color-primary)',
+        secondary: 'var(--color-secondary)',
+        ice: 'var(--color-ice)',
+        bluePrimary: 'var(--color-bluePrimary)',
+      },
+      fontFamily: {
+        inter: ['var(--font-inter)'],
+      },
+    },
+  },
+}
